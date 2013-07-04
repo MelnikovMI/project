e5ede6ddb896a49cf90aca1364e4549d43fa2cd3
MelnikovMI <Alpha20@yandex.ru>
2013-07-04 07:24:05 +0400

    C12

--- proj/MainWindow.xaml.cs
+++ proj/MainWindow.xaml.cs
@@ -164,5 +164,50 @@ namespace proj
             e.Finish();
 
         }
+
+
+        public void save_word(double x)
+        {
+            Word.Application wordApplication = new Word.Application(); //объявили переменную типа Word
+            Object template = Type.Missing;
+            Object newTemplate = Type.Missing;
+            Object documentType = Type.Missing;
+            Object visible = Type.Missing;
+            wordApplication.Documents.Add(ref template, ref newTemplate, ref documentType, ref visible);//добавили в проложение документ
+            Word.Document doc = wordApplication.ActiveDocument;
+            //wordApplication.Visible = true; //делаем что бы word не работал в фоновом режиме
+            Object r = Type.Missing;
+            Word.Paragraph par = doc.Content.Paragraphs.Add(ref r);//дабавляем в документ параграф
+            Object missing = Type.Missing;
+            Word.Range rng = doc.Range(ref missing, ref missing); //получаем текстовую область параграфа
+            rng.Tables.Add(doc.Paragraphs[doc.Paragraphs.Count].Range, 9, 1, ref missing, ref missing);//вставляем в текстовую область таблицу
+            Word.Table tbl = doc.Tables[doc.Tables.Count];//для удобства работы присваиваем таблицу переменной
+            tbl.Cell(1, 1).Range.Paragraphs.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+            tbl.Cell(1, 1).Range.Text = "МЕТОД АДАМСА ДЛЯ РЕШЕНИЯ ОБЫНОВЕННЫХ ДИФФЕРЕНЦИАЛЬНЫХ УРАВНЕНИЙ";
+            tbl.Cell(2, 1).Range.Paragraphs.Alignment = Word.WdParagraphAlignment.wdAlignParagraphLeft;
+            tbl.Cell(2, 1).Range.Text = "Вы ввели уравнение f(" + variable + ")=" + funk;
+            tbl.Cell(3, 1).Range.Paragraphs.Alignment = Word.WdParagraphAlignment.wdAlignParagraphLeft;
+            tbl.Cell(3, 1).Range.Text = "Левая граница = " + a;
+            tbl.Cell(4, 1).Range.Paragraphs.Alignment = Word.WdParagraphAlignment.wdAlignParagraphLeft;
+            tbl.Cell(4, 1).Range.Text = "Правая граница = " + b;
+            tbl.Cell(5, 1).Range.Paragraphs.Alignment = Word.WdParagraphAlignment.wdAlignParagraphLeft;
+            tbl.Cell(5, 1).Range.Text = "x0 = " + X0;
+            tbl.Cell(6, 1).Range.Paragraphs.Alignment = Word.WdParagraphAlignment.wdAlignParagraphLeft;
+            tbl.Cell(6, 1).Range.Text = "y0 = " + Y0;
+            tbl.Cell(7, 1).Range.Paragraphs.Alignment = Word.WdParagraphAlignment.wdAlignParagraphLeft;
+            tbl.Cell(7, 1).Range.Text = "Ответ: " + variable + " : ";
+            tbl.Cell(8, 1).Range.Paragraphs.Alignment = Word.WdParagraphAlignment.wdAlignParagraphLeft;
+            for (int i = 0; i < num_points; i++)
+            {
+                tbl.Cell(8, i).Range.Text = "( " + Convert.ToString(myX[i]) + " , " + Convert.ToString(myY[i]) + " )";
+            }
+            object fileName = Path + @"Решение.doc";
+            doc.SaveAs(ref fileName,
+            ref missing, ref missing, ref missing, ref missing, ref missing,
+            ref missing, ref missing, ref missing, ref missing, ref missing,
+            ref missing, ref missing, ref missing, ref missing, ref missing);
+            //doc.Close(ref missing, ref missing, ref missing);
+            //wordApplication.Quit();
+        }
     }
 }
